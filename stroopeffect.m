00db550f118a63fa4d61e4361eed224d25136b26
ssood123 <38160599+ssood123@users.noreply.github.com>
2018-07-03 13:14:23 -0400

    Changing the name of the .dat file
    -The .dat file name now has the person's last name, person's first name, and the date the .dat file was created
    -Optimized code and deleted unnecessary variables

--- stroopeffect.m
+++ stroopeffect.m
@@ -7,17 +7,28 @@ Screen('Preference', 'SkipSyncTests', 2);
 Screen('Preference','VisualDebugLevel', 1);
 [window, window_size] = Screen('OpenWindow', 0, [255 255 255], [],32,2);
 
+%FILE MAKING
+last = input('Enter your last name: ','s');
+last = strtrim(last);
+first = input('Enter your first name: ','s');
+first = strtrim(first);
+t = datetime;
+DateString = datestr(t);
+[DateString, ~] = strtok(DateString);
+nameoffile = strcat(last,first);
+nameoffile = strcat(nameoffile,DateString);
+nameoffile = strcat(nameoffile,'.dat');
+fid = fopen(nameoffile,'w');
+
 %VARIABLES
-waittime = .5;
 randomColors = {'Red','Orange', 'Yellow', 'Green', 'Blue', 'Purple'};
 randomColors2 = {[255 0 0], [255 165 0], [255 255 0], [0 255 0], [0 0 255], [75 0 130]};
-times = {'2','1'};
-correctanswerslist1 = [];
-correctanswerslist2 = [];
-timeslist1 = [];
-timeslist2 = [];
-colorslist = {};
-wordslist = {};
+keyRed = KbName('r');
+keyOrange = KbName('o');
+keyYellow = KbName('y');
+keyGreen = KbName('g');
+keyBlue = KbName('b');
+keyPurple = KbName('p');
 
 %DISPLAYS EACH WORD AND ITS OWN COLOR
 for i = 1:2
@@ -25,39 +36,37 @@ for i = 1:2
     Screen(window,'TextSize',300);
     word = randomColors{i};
     color = randomColors2{i};
-    wordslist{end+1} = word;
-    colorslist{end+1} = randomColors{i};
     DrawFormattedText(window, word,'center', 'center', color,[],[],[],2);
+    color = randomColors{i};
     Screen('Flip',window);
     WaitSecs(.2);
-    [secs, keyCode, deltaSecs] = KbWait;
+    tic
+    [~, keyCode, ~] = KbWait;
+    toc
+    time = toc;
     answer = 0;
-     if keyCode(21) == 1
-        answer = [255 0 0];
-    elseif keyCode(18) == 1
-        answer = [255 165 0];
-    elseif keyCode(28) == 1
-        answer = [255 255 0];
-    elseif keyCode(10) == 1
-        answer = [0 255 0];
-    elseif keyCode(5) == 1
-        answer = [0 0 255];
-    elseif keyCode(19) == 1
-        answer = [75 0 130];
+    if keyCode(keyRed) == 1
+        answer = 'Red';
+    elseif keyCode(keyOrange) == 1
+        answer = 'Orange';
+    elseif keyCode(keyYellow) == 1
+        answer = 'Yellow';
+    elseif keyCode(keyGreen) == 1
+        answer = 'Green';
+    elseif keyCode(keyBlue) == 1
+        answer = 'Blue';
+    elseif keyCode(keyPurple) == 1
+        answer = 'Purple';
     end
-
     if isequal(answer,color)
-        correctanswerslist1(end+1) = 1;
+        correctanswer = 1;
     else
-        correctanswerslist1(end+1) = 0;
+        correctanswer = 0;
     end
-    timeslist1(end+1) = deltaSecs;
-    for j = 1:1
-        %DrawFormattedText(window,times{j},window_size(3)/2, window_size(4)/2+50, [0 0 0],[],[],[],2);
-        Screen('FillOval', window , [0 0 0], [1280/2-40,720/2-40,1280/2+40,720/2+40]);
-        Screen('Flip',window);
-        WaitSecs(1);
-    end
+    fprintf(fid,'%s %s %f %d\n',word,color,time,correctanswer);
+    Screen('FillOval', window , [0 0 0], [1280/2-40,720/2-40,1280/2+40,720/2+40]);
+    Screen('Flip',window);
+    WaitSecs(1);
 end
     
 
@@ -72,50 +81,37 @@ for i = 1:2
     Screen(window,'TextSize',300);
     word = randomColors{pickone};
     color = randomColors2{pickanother};
-    wordslist{end+1} = word;
-    colorslist{end+1} = randomColors{pickanother};
     DrawFormattedText(window, word,'center', 'center', color,[],[],[],2);
+    color = randomColors{pickanother};
     Screen('Flip',window);
     WaitSecs(.2);
-    [secs, keyCode, deltaSecs] = KbWait;
-    if keyCode(21) == 1
-        answer = [255 0 0];
-    elseif keyCode(18) == 1
-        answer = [255 165 0];
-    elseif keyCode(28) == 1
-        answer = [255 255 0];
-    elseif keyCode(10) == 1
-        answer = [0 255 0];
-    elseif keyCode(5) == 1
-        answer = [0 0 255];
-    elseif keyCode(19) == 1
-        answer = [75 0 130];
+    tic
+    [~, keyCode, ~] = KbWait;
+    toc
+    time = toc;
+    if keyCode(keyRed) == 1
+        answer = 'Red';
+    elseif keyCode(keyOrange) == 1
+        answer = 'Orange';
+    elseif keyCode(keyYellow) == 1
+        answer = 'Yellow';
+    elseif keyCode(keyGreen) == 1
+        answer = 'Green';
+    elseif keyCode(keyBlue) == 1
+        answer = 'Blue';
+    elseif keyCode(keyPurple) == 1
+        answer = 'Purple';
     end
-
     if isequal(answer,color)
-        correctanswerslist2(end+1) = 1;
+        correctanswer = 1;
     else
-        correctanswerslist2(end+1) = 0;
+        correctanswer = 0;
     end
-    timeslist2(end+1) = deltaSecs;
-    for j = 1:1
-        %DrawFormattedText(window,times{j},'center', 'center'-50, [0 0 0],[],[],[],2);
-        %DrawFormattedText(window,times{j},window_size(3)/2, window_size(4)/2+50, [0 0 0],[],[],[],2);
-        Screen('FillOval', window , [0 0 0], [1280/2-40,720/2-40,1280/2+40,720/2+40]);
-        Screen('Flip',window);
-        WaitSecs(1);
-    end
-end
+    fprintf(fid,'%s %s %f %d\n',word,color,time,correctanswer);
+    Screen('FillOval', window , [0 0 0], [1280/2-40,720/2-40,1280/2+40,720/2+40]);
+    Screen('Flip',window);
+    WaitSecs(1);
+ end
 
 Screen('CloseAll');
-correctanswerslist = [correctanswerslist1 correctanswerslist2];
-timeslist = [timeslist1 timeslist2];
-nameoffile = input('Enter your info in this format: lastnamefirstname: ','s');
-nameoffile = strtrim(nameoffile);
-nameoffile = strcat(nameoffile,'.dat');
-fid = fopen(nameoffile,'w');
-for i = 1:length(colorslist)
-    fprintf(fid,'%s %s %f %d\n',wordslist{i},colorslist{i},timeslist(i),correctanswerslist(i));
-end
 fclose(fid);
-

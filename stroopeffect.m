724a7bffb0b165b1fbe2561b9a2881a918a9b018
ssood123 <38160599+ssood123@users.noreply.github.com>
2018-07-03 12:44:04 -0400

    Storing data for each word and color display
    -The reaction times of the user are stored in a vector (The reaction time is the time it takes for a user to press a key right after a word and its color are displayed)
    -Whether or not the user pressed the right key (the user is supposed to press the a key corresponding to the color of the word (for example, 'r' for a red colored word)) is stored in a vector
    -Right after a user presses a key, a 2-1 timer is shown to prepare the user for the next word and color display

--- stroopeffect.m
+++ stroopeffect.m
@@ -11,19 +11,51 @@ Screen('Preference','VisualDebugLevel', 1);
 waittime = .5;
 randomColors = {'Red','Orange', 'Yellow', 'Green', 'Blue', 'Purple'};
 randomColors2 = {[255 0 0], [255 165 0], [255 255 0], [0 255 0], [0 0 255], [75 0 130]};
+times = {'2','1'};
+correctanswerslist1 = [];
+correctanswerslist2 = [];
+timeslist1 = [];
+timeslist2 = [];
 
 %DISPLAYS EACH WORD AND ITS OWN COLOR
-for i = 1:6
+for i = 1:2
     Screen(window,'TextFont','Arial');
     Screen(window,'TextSize',300);
-    DrawFormattedText(window, randomColors{i},'center', 'center', randomColors2{i},[],[],[],2);
+    word = randomColors{i};
+    color = randomColors2{i};
+    DrawFormattedText(window, word,'center', 'center', color,[],[],[],2);
     Screen('Flip',window);
-    KbWait;
-    WaitSecs(waittime);
+    WaitSecs(.2);
+    [secs, keyCode, deltaSecs] = KbWait;
+    if keyCode(21) == 1
+        answer = [255 0 0];
+    elseif keyCode(18) == 1
+        answer = [255 165 0];
+    elseif keyCode(28) == 1
+        answer = [255 255 0];
+    elseif keyCode(10) == 1
+        answer = [0 255 0];
+    elseif keyCode(5) == 1
+        answer = [0 0 255];
+    elseif keyCode(19) == 1
+        answer = [75 0 130];
+    end
+    if isequal(answer,color)
+        correctanswerslist1(end+1) = 1;
+    else
+        correctanswerslist1(end+1) = 0;
+    end
+    timeslist1(end+1) = deltaSecs;
+    for j = 1:2
+        DrawFormattedText(window,times{j},'center', 'center', [0 0 0],[],[],[],2);
+        Screen('Flip',window);
+        WaitSecs(1);
+    end
 end
+    
 
 %DISPLAYS A RANDOM WORD WITH A RANDOM COLOR
-for i = 1:10
+for i = 1:2
     pickone = randi([1,6]);
     pickanother = randi([1,6]);
     while pickanother == pickone
@@ -31,10 +63,36 @@ for i = 1:10
     end
     Screen(window,'TextFont','Arial');
     Screen(window,'TextSize',300);
-    DrawFormattedText(window, randomColors{pickone},'center', 'center', randomColors2{pickanother},[],[],[],2);
+    word = randomColors{pickone};
+    color = randomColors2{pickanother};
+    DrawFormattedText(window, word,'center', 'center', color,[],[],[],2);
     Screen('Flip',window);
-    KbWait;
-    WaitSecs(waittime);
+    WaitSecs(.2);
+    [secs, keyCode, deltaSecs] = KbWait;
+    if keyCode(21) == 1
+        answer = [255 0 0];
+    elseif keyCode(18) == 1
+        answer = [255 165 0];
+    elseif keyCode(28) == 1
+        answer = [255 255 0];
+    elseif keyCode(10) == 1
+        answer = [0 255 0];
+    elseif keyCode(5) == 1
+        answer = [0 0 255];
+    elseif keyCode(19) == 1
+        answer = [75 0 130];
+    end
+    if isequal(answer,color)
+        correctanswerslist2(end+1) = 1;
+    else
+        correctanswerslist2(end+1) = 0;
+    end
+    timeslist2(end+1) = deltaSecs;
+    for j = 1:2
+        DrawFormattedText(window,times{j},'center', 'center', [0 0 0],[],[],[],2);
+        Screen('Flip',window);
+        WaitSecs(1);
+    end
 end
  
 Screen('CloseAll');

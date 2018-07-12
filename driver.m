d250c1b5780cc045dfe9cae0099e7635bfe180db
ssood123 <38160599+ssood123@users.noreply.github.com>
2018-07-12 12:19:05 -0400

    Create driver.m
    -This is the overarching program for plotting.
    -For the first 6 iterations (first for loop), the driver program determines the markers for each of the word and congruent color displays and graphs the data in the boundary determined by the two markers for the word and color displays by calling analysis.m.
    -The last 6 iterations (second for loop) do the same thing as the first six iterations except for word and incongruent color displays and using analysis2.m.

---
+++ driver.m
@@ -0,0 +1,58 @@
+clc;
+clear;
+
+file = input('name of session list-->', 's');
+fid = fopen([file '.asc']);
+
+line_count = 1;
+while feof(fid) == 0
+    line_data{line_count} = fgetl(fid);
+    line_count = line_count +1 ;
+end
+line_count = line_count-1;
+
+for a = 1:6
+  start_index = 1;
+  end_index = 1;
+  
+  for index = 1:line_count
+    str = strfind(line_data(index), strcat('C-Test',num2str(a)));
+    if ~isempty(str{1})
+        start_index = index;
+        break;
+    end
+  end
+  
+  for index = 1:line_count
+    str = strfind(line_data(index), strcat('EC-Test',num2str(a)));
+    if ~isempty(str{1})
+        end_index = index;
+        break;
+    end
+  end
+  
+  analysis
+end
+
+for a = 1:6
+    start_index = 1;
+  end_index = 1;
+  
+  for index = 1:line_count
+    str = strfind(line_data(index), strcat('I-Test',num2str(a)));
+    if ~isempty(str{1})
+        start_index = index;
+        break;
+    end
+  end
+  
+  for index = 1:line_count
+    str = strfind(line_data(index), strcat('EI-Test',num2str(a)));
+    if ~isempty(str{1})
+        end_index = index;
+        break;
+    end
+  end
+  
+  analysis2
+end

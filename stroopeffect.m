1b7d10eaf756558844e125cdc6b92c6509b8793e
ssood123 <38160599+ssood123@users.noreply.github.com>
2018-07-12 11:56:21 -0400

    Included markers in the .asc file
    -The .asc file generated from the EDF file generated by this code from the eye tracker now has markers for each word and color display corresponding to the time a word and color display shows and the time when the user presses a key after the word and color display shows.
    -An example of a marker is 'C-Test Red Red'. This marker corresponds to the time of the congruent color ('C' for congruent) display (red word, red color). The time the user presses a key would have marker 'EC-Test Red Red', where the 'E' stands for 'end'.

--- stroopeffect.m
+++ stroopeffect.m
@@ -15,11 +15,13 @@ close all;
     nameoffile = strcat(nameoffile,DateString);
     nameoffile = strcat(nameoffile,'.dat');
     fid = fopen(nameoffile,'w');
-    
-    PsychDefaultSetup(2);
-    commandwindow;
-    dummymode = 0;
-    
+commandwindow;
+dummymode = 0;    
+PsychDefaultSetup(2);
+Screen('Preference', 'SkipSyncTests', 0);
+screens = Screen('Screens');
+screenNumber = max(screens);
+rand('seed', sum(100 * clock));
     prompt = {'Enter tracker EDF file name (1 to 8 letters or numbers)'};
     dlg_title = 'Create EDF file';
     num_lines= 1;
@@ -28,12 +30,18 @@ close all;
     edfFile = answer{1};
     fprintf('EDFFile: %s\n', edfFile );
 
+white = WhiteIndex(screenNumber);
+black = BlackIndex(screenNumber);
+
+rng('default');
 rng('shuffle');
 
-Screen('Preference', 'SkipSyncTests', 2);
+% Screen('Preference', 'SkipSyncTests', 2);
 Screen('Preference','VisualDebugLevel', 1);
 [window, window_size] = Screen('OpenWindow', 0, [255 255 255], [],32,2);
+Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
+start = GetSecs;
 
 %VARIABLES
 randomColors = {'Red','Orange', 'Yellow', 'Green', 'Blue', 'Purple'};
@@ -47,7 +55,7 @@ keyPurple = KbName('p');
 shuffler = [1 2 3 4 5 6];
 shuffler = shuffler(randperm(length(shuffler)));
 
-
+%start = GetSecs; %new stuff
 
 el=EyelinkInitDefaults(window);
 if ~EyelinkInit(dummymode, 1)
@@ -81,7 +89,7 @@ EyelinkUpdateDefaults(el);
 HideCursor;
 % Calibrate the eye tracker
 EyelinkDoTrackerSetup(el);    
-
+n = 'Test begin';
 DrawFormattedText(window, 'Press a key to begin the test'...
     , 'center', 'center', white);
  Screen('Flip', window);
@@ -92,33 +100,33 @@ KbStrokeWait;
     % clear tracker display and draw box at center
     Eyelink('Command', 'clear_screen %d', 0);
 Eyelink('message', 'SYNCTIME')
-Eyelink('message', ['NEWTRIAL',' ',num2str((h))]);
+%Eyelink('message', ['NEWTRIAL',' ',num2str((h))]);
 EyelinkDoDriftCorrection(el);
 WaitSecs(0.1);
     Eyelink('StartRecording');
     WaitSecs(0.1);
+%     
+%   if Eyelink('IsConnected')~=1 && ~dummymode
+%         
+%         cleanup;
+%         return;
+%   end
     
-  if Eyelink('IsConnected')~=1 && ~dummymode
-        
-        cleanup;
-        return;
-  end
-    
-Screen('FillRect', window, black);
-Eyelink('message', ['NEWTRIAL',' ',num2str((h))]);
-Eyelink('startrecording');
+% Screen('FillRect', window, black);
+% Eyelink('message', ['NEWTRIAL',' ',num2str((h))]);
+% Eyelink('startrecording');
 
-            Eyelink('message', ['NO_RECORD',' ',num2str((p))]);
-
- Screen('DrawDots', window, [xPosVector ; yPosVector ],...
-                dotSizes2, grey, dotCenter, 0); %ORIGINAL DRAWDOTS
-
-            Eyelink('message', ['RESUME_MOTION',' ',num2str((r))]);
-
-        Screen('DrawDots', window, [xPosVector; yPosVector ],...
-            dotSizes2, grey, dotCenter, 0);
-
-            Eyelink('message', ['FREEZE',' ',num2str((f))]);
+%             Eyelink('message', ['NO_RECORD',' ',num2str((p))]);
+% 
+%  Screen('DrawDots', window, [xPosVector ; yPosVector ],...
+%                 dotSizes2, grey, dotCenter, 0); %ORIGINAL DRAWDOTS
+% 
+%             Eyelink('message', ['RESUME_MOTION',' ',num2str((r))]);
+% 
+%         Screen('DrawDots', window, [xPosVector; yPosVector ],...
+%             dotSizes2, grey, dotCenter, 0);
+% 
+%             Eyelink('message', ['FREEZE',' ',num2str((f))]);
             
 %DISPLAYS INITIAL FIXATION POINT
 Screen(window,'TextFont','Arial');
@@ -130,16 +138,44 @@ WaitSecs(1);
 %DISPLAYS EACH WORD AND ITS OWN COLOR
 for i = shuffler
     Screen(window,'TextFont','Arial');
-    Screen(window,'TextSize',300);
+    Screen(window,'TextSize',200);
     word = randomColors{i};
     color = randomColors2{i};
     DrawFormattedText(window, word,'center', 'center', color,[],[],[],2);
     color = randomColors{i};
     Screen('Flip',window);
-    WaitSecs(.2);
+    switch i
+        case 1
+            Eyelink('message', 'C-Test RED RED');
+        case 2
+            Eyelink('message', 'C-Test ORANGE ORANGE');
+        case 3
+            Eyelink('message', 'C-Test YELLOW YELLOW');
+        case 4
+            Eyelink('message', 'C-Test GREEN GREEN');
+        case 5
+            Eyelink('message', 'C-Test BLUE BLUE');
+        case 6
+            Eyelink('message', 'C-Test PURPLE PURPLE');
+    end
+            %WaitSecs(.2);
     tic
     [~, keyCode, ~] = KbWait;
     time = toc;
+    switch i
+        case 1
+            Eyelink('message', 'EC-Test RED RED');
+        case 2
+            Eyelink('message', 'EC-Test ORANGE ORANGE');
+        case 3
+            Eyelink('message', 'EC-Test YELLOW YELLOW');
+        case 4
+            Eyelink('message', 'EC-Test GREEN GREEN');
+        case 5
+            Eyelink('message', 'EC-Test BLUE BLUE');
+        case 6
+            Eyelink('message', 'EC-Test PURPLE PURPLE');
+    end
     answer = 0;
     if keyCode(keyRed) == 1
         answer = 'Red';
@@ -174,16 +210,20 @@ for i = 1:6
         pickanother = randi([1,6]);
     end
     Screen(window,'TextFont','Arial');
-    Screen(window,'TextSize',300);
+    Screen(window,'TextSize',200);
     word = randomColors{pickone};
     color = randomColors2{pickanother};
     DrawFormattedText(window, word,'center', 'center', color,[],[],[],2);
     color = randomColors{pickanother};
     Screen('Flip',window);
-    WaitSecs(.2);
+    text = ['I-Test' ' ' upper(word) ' ' upper(color)];
+    Eyelink('message', text);
+    %WaitSecs(.2);
     tic
     [~, keyCode, ~] = KbWait;
     time = toc;
+    text = ['EI-Test' ' ' upper(word) ' ' upper(color)];
+    Eyelink('message', text);
     if keyCode(keyRed) == 1
         answer = 'Red';
     elseif keyCode(keyOrange) == 1

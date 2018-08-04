c387118ec9c27fc9beffb8848fe5905dfc12588a
ssood123 <38160599+ssood123@users.noreply.github.com>
2018-08-04 15:13:34 -0400

    Delete savefigures.m

--- savefigures.m
+++
@@ -1,6 +0,0 @@
-startingfigure = input('What is the starting figure? ');
-endingfigure = input('What is the ending figure? ');
-nameextension = input('Enter the name extension: ','s');
-for i = startingfigure:endingfigure
-    saveas(figure(i),strcat('Figure',num2str(i),nameextension),'jpg')
-end
